f475de5ec40dd752b95cd74458acf85575bfa753
Larissa <larissanunesvaz23@gmail.com>
2025-05-24 12:45:15 -0300

    adicionada seção de testemunhos e FAQ

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { HeroSlider } from "@/components/hero-slider"
 import { StatsSection } from "@/components/stats-section"
 import { DonationPoints } from "@/components/donation-points"
+import { TestimonialsSection } from "@/components/testemonials"
+import { FaqSection } from "@/components/faq"
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <HeroSlider />
       <StatsSection />
       <DonationPoints />
+      <TestimonialsSection/>
+      <FaqSection/>
     </main>
   )
 }
